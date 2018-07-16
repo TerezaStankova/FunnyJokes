5780618e418623cda9a244af78747561fe855dd9
Tereza <tereza.stankova1@gmail.com>
2018-07-16 13:13:42 +0200

    style: add color and margin

--- JokeLibrary/src/main/java/com/example/android/jokelibrary/MyJoker.java
+++ JokeLibrary/src/main/java/com/example/android/jokelibrary/MyJoker.java
@@ -3,10 +3,10 @@ package com.example.android.jokelibrary;
 public class MyJoker {
     public static String getJoke() {
         String text = "Q. How do you make the number one disappear?\n" +
-                "A. Add the letter G and it's Gone!" + "\n" +
+                "A. Add the letter G and it's Gone!\n" + "\n" + "\n" +
                 "Q: Did you hear the one about the statistician?\n" +
-                "A: Probably." + "\n" + "Q: Why do mathematicians like parks?\n" +
-                "A: Because of all the natural logs.";
+                "A: Probably.\n" + "\n" + "\n" + "Q: Why do mathematicians like parks?\n" +
+                "A: Because of all the natural logs.\n";
         return text;
     }
 }
