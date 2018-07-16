72c296add1767820140093bd894fff6dedc8ef37
Tereza <tereza.stankova1@gmail.com>
2018-07-16 11:08:18 +0200

    feat: add more jokes

--- JokeLibrary/src/main/java/com/example/android/jokelibrary/MyJoker.java
+++ JokeLibrary/src/main/java/com/example/android/jokelibrary/MyJoker.java
@@ -2,8 +2,11 @@ package com.example.android.jokelibrary;
 
 public class MyJoker {
     public static String getJoke() {
-        String text = "Q. How do you make the number one disappear?\n" + "\n" +
-                "A. Add the letter G and it's Gone!";
+        String text = "Q. How do you make the number one disappear?\n" +
+                "A. Add the letter G and it's Gone!" + "\n" +
+                "Q: Did you hear the one about the statistician?\n" +
+                "A: Probably." + "\n" + "Q: Why do mathematicians like parks?\n" +
+                "A: Because of all the natural logs.";
         return text;
     }
 }
